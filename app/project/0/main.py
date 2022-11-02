8df21308f667117242ecc566a676d4bc72699125
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-11-02 01:07:19 -0500

    Swap http.server w/sanic
    async, gunicorn integration, and most importantly, auto reload

--- app/project/0/main.py
+++ app/project/0/main.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 
 # fmt: off
-import http.server
-# from pathlib import Path
+import os
+from pathlib import Path
+from sanic import Sanic
 # fmt: on
 
+# env vars
+if os.getenv("APP_SERVICE_PORT") is not None:
+    PORT = int(os.getenv("APP_SERVICE_PORT"))
+else:
+    PORT = 8000
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/":
-            self.path = "/index.html"
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+# init app
+app = Sanic(__name__)
+
+if Path("index.html").exists():
+    app.static("/", "./index.html", name="index")
+
+app.static("/", ".")
 
 
 if __name__ == "__main__":
-    """Basic HTTP server that serves files from the current directory."""
-    http.server.test(HandlerClass=Handler)
+    """
+    Run the server app
+
+    dev: debug=True, auto_reload=True
+    access_log: False for performance
+    motd: False to hide the banner
+    """
+
+    app.run(host="0.0.0.0", port=PORT, dev=True, access_log=False, motd=False)
